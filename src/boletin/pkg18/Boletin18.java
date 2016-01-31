da695fd6856de511c3679d7532d88ef6af938213
Manuel <Manuel@SNOW-PC>
2016-01-31 22:21:20 +0100

    start

---
+++ src/boletin/pkg18/Boletin18.java
@@ -0,0 +1,14 @@
+package boletin.pkg18;
+public class Boletin18 {
+
+    public static void main(String[] args) {
+        claseArrays num = new claseArrays();
+        num.cargarArray();
+        num.visualizar();
+        
+        
+               
+       
+    }
+    
+}
